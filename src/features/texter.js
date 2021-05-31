7e2e0cbc392d01ec27e5999a790be29433b22a65
gunnarsdotter <gunnarsdotter@live.com>
2021-05-31 12:31:27 +0200

    Updates

--- src/features/texter.js
+++
@@ -1,6 +0,0 @@
-export const nyckeltalstext = "Här ska det stå en text som informerar om hur det fungerar!";
-export const stapeldiagramstext = "Här ska det stå en text som informerar om hur det fungerar!";
-export const textrutatext = "Här ska det stå en text som informerar om hur det fungerar!";
-export const multistapeldiagramtext = "Här ska det stå en text som informerar om hur det fungerar!";
-export const linjegrafstext = "Här ska det stå en text som informerar om hur det fungerar!3";
-export const storsta = "Här ska det stå en text som informerar om hur det fungerar!";
